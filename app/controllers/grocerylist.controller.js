267b47a738ecad2a24d467c197cd32ab478b2cf2
Dawit Abera <dawitabera64@gmail.com>
2018-11-04 11:51:02 -0800

    Controller class  - added controller functions

---
+++ app/controllers/grocerylist.controller.js
@@ -0,0 +1,26 @@
+const Note = require('../models/grocerylist.model');
+
+// Create and Save a new grocery-list
+exports.create = (req, res) => {
+
+};
+
+// Retrieve and return all grocery-list from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single grocery-list with a listId
+exports.findOne = (req, res) => {
+
+};
+
+// Update a grocery-list identified by the listId in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a grocery-list with the specified listId in the request
+exports.delete = (req, res) => {
+
+};
